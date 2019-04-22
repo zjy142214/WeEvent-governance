f52555b49f4eb73519202f5802683690e794a9ea
v_wbjnzhang <v_wbjnzhang@v_wbjnzhang-PC.webank.com>
2019-04-22 14:26:08 +0800

    remove sso part

--- src/main/java/com/webank/weevent/governance/configuration/WeeventConfiguration.java
+++ src/main/java/com/webank/weevent/governance/configuration/WeeventConfiguration.java
@@ -1,21 +1,12 @@
 package com.webank.weevent.governance.configuration;
 
-import org.jasig.cas.client.authentication.AuthenticationFilter;
-import org.jasig.cas.client.session.SingleSignOutFilter;
-import org.jasig.cas.client.session.SingleSignOutHttpSessionListener;
-import org.jasig.cas.client.util.AssertionThreadLocalFilter;
-import org.jasig.cas.client.util.HttpServletRequestWrapperFilter;
-import org.jasig.cas.client.validation.Cas20ProxyReceivingTicketValidationFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-import com.webank.weevent.governance.filter.CORSFilter;
-import com.webank.weevent.governance.filter.LoginHelperFilter;
 import com.webank.weevent.governance.filter.XssFilter;
 
 @Configuration
@@ -68,87 +59,15 @@ public class WeeventConfiguration {
         return factory;
     }
     
-    @Bean
-    public FilterRegistrationBean<CORSFilter> crosRegist() {
-        FilterRegistrationBean<CORSFilter> frBean = new FilterRegistrationBean<>();
-        frBean.setFilter(new CORSFilter());
-        frBean.addUrlPatterns("/*");
-        frBean.setOrder(1);
-        return frBean;
-    }
-    
-    @Bean
-    public FilterRegistrationBean<SingleSignOutFilter> singleSignOutRegist() {
-        FilterRegistrationBean<SingleSignOutFilter> frBean = new FilterRegistrationBean<>();
-        frBean.setFilter(new SingleSignOutFilter());
-        frBean.addUrlPatterns("/*");
-        frBean.setOrder(2);
-        return frBean;
-    }
-    
-    @Bean
-    public FilterRegistrationBean<AuthenticationFilter> authenticationFilterRegist() {
-        FilterRegistrationBean<AuthenticationFilter> frBean = new FilterRegistrationBean<>();
-        frBean.setFilter(new AuthenticationFilter());
-        frBean.addUrlPatterns("/*");
-        frBean.setOrder(3);
-        return frBean;
-    }
-    
-    @Bean
-    public FilterRegistrationBean<Cas20ProxyReceivingTicketValidationFilter> cas20ProxyReceivingTicketValidationFilterRegist() {
-        FilterRegistrationBean<Cas20ProxyReceivingTicketValidationFilter> frBean = new FilterRegistrationBean<>();
-        frBean.setFilter(new Cas20ProxyReceivingTicketValidationFilter());
-        frBean.addInitParameter("hostnameVerifier", "org.jasig.cas.client.ssl.AnyHostnameVerifier");
-        frBean.addUrlPatterns("/*");
-        frBean.setOrder(4);
-        return frBean;
-    }
-    
-    @Bean
-    public FilterRegistrationBean<LoginHelperFilter> loginHelperFilterRegist() {
-        FilterRegistrationBean<LoginHelperFilter> frBean = new FilterRegistrationBean<>();
-        frBean.setFilter(new LoginHelperFilter());
-        frBean.addUrlPatterns("/*");
-        frBean.setOrder(5);
-        return frBean;
-    }
-    
-    @Bean
-    public FilterRegistrationBean<HttpServletRequestWrapperFilter> httpServletRequestWrapperFilterRegist() {
-        FilterRegistrationBean<HttpServletRequestWrapperFilter> frBean = new FilterRegistrationBean<>();
-        frBean.setFilter(new HttpServletRequestWrapperFilter());
-        frBean.setOrder(6);
-        frBean.addUrlPatterns("/*");
-        return frBean;
-    }
-    
-    @Bean
-    public FilterRegistrationBean<AssertionThreadLocalFilter> assertionThreadLocalFilterRegist() {
-        FilterRegistrationBean<AssertionThreadLocalFilter> frBean = new FilterRegistrationBean<>();
-        frBean.setFilter(new AssertionThreadLocalFilter());
-        frBean.addUrlPatterns("/*");
-        frBean.setOrder(7);
-        return frBean;
-    }
     
     @Bean
     public FilterRegistrationBean<XssFilter> xssFilterRegistrationBean() {
         FilterRegistrationBean<XssFilter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new XssFilter());
-        filterRegistrationBean.setOrder(8);
+        filterRegistrationBean.setOrder(1);
         filterRegistrationBean.setEnabled(true);
         filterRegistrationBean.addUrlPatterns("/*");
         return filterRegistrationBean;
-    }
-
-    
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    @Bean
-    public ServletListenerRegistrationBean listenerRegist() {
-        ServletListenerRegistrationBean srb = new ServletListenerRegistrationBean();
-        srb.setListener(new SingleSignOutHttpSessionListener());
-        return srb;
     }
     
 }
