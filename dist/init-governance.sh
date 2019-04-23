fff7e8b56a0b5c8d64e4bbc430fc3a61a9761aa8
zjy142214 <zjy142214@163.com>
2019-04-23 21:14:08 +0800

    update init-goverance style

--- dist/init-governance.sh
+++ dist/init-governance.sh
@@ -6,7 +6,7 @@ java -Xbootclasspath/a:./conf -cp ./apps/*  -Dloader.main=com.webank.weevent.gov
 if [ $? -ne 0 ];then
     echo "init governance db failed"
     echo "please check cofiguration of mysql"
-	return 1
+    return 1
 else
    echo "init governance db success"
 fi
